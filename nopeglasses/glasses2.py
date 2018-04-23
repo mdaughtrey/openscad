beb987c8fad82d5cab78c451deee5330d9c7ab4e
matt@daughtrey.com <matt@daughtrey.com>
2018-04-22 22:22:50 -0400

    bmp

--- nopeglasses/glasses2.py
+++ nopeglasses/glasses2.py
@@ -4,9 +4,11 @@ import numpy
 #import imageio
 import imageio
 from PIL import Image
+import collections
 import pdb
 
 def loadBmp():
+    plotlist = collections.deque()
     bmp = numpy.rot90(imageio.imread("./glasses2.bmp"))
     offset=0
     #mult = 16
@@ -17,12 +19,18 @@ def loadBmp():
     #print("polygon(points=[[0,0],[10,0],")
     print("polygon(points=[[0,0],")
     for ii in bmp:
-        first1 = numpy.argwhere(ii)
-        if 0 == len(first1):
-            print("[",int(index),",0],",sep="",end=""),
-        else:
-            print("[",int(index),",",first1[0][0]*mult+offset,"],",sep="",end=""),
+        whites = numpy.argwhere(ii)
+        if 0 == len(whites):
+            continue
+            #print("[",int(index),",0],",sep="",end=""),
+        plotlist.append((index, whites[0][0]))
+        plotlist.appendleft((index, whites[-1][0]))
+        #else:
+        #    print("[",int(index),",",first1[0][0]*mult+offset,"],",sep="",end=""),
         index = index+inc
-    print("[",int(index),",0],[",int(index)+10,",0]],convexity=1);",sep="")
+    for xx in plotlist:
+        print("[",xx[0],",",xx[1],"],",sep="",end="")
+    print("],convexity=1);")
+    #print("[",int(index),",0],[",int(index)+10,",0]],convexity=1);",sep="")
 
 loadBmp()
